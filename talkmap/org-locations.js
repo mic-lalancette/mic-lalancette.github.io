1faab68c6cf1b13db42872ef118359545da10772
MicLalancette <61370608+mic-lalancette@users.noreply.github.com>
2022-07-08 00:39:06 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,48 +5,43 @@ var addressPoints = [
     -73.5791162596496
   ],
   [
-    "Fields Institute, Toronto, Canada",
-    43.65879595,
-    -79.39755040601011
-  ],
-  [
-    "University of Zagreb, Zagreb, Croatia",
-    45.8644842,
-    15.8005897
-  ],
-  [
-    "Universit\u00e9 de Montr\u00e9al, Montr\u00e9al, Canada",
-    45.505536,
-	-73.613864
-  ],
-  [
-    "Fields Institute, Toronto, Canada",
-    43.65879595,
-    -79.39755040601011
-  ],
-  [
-    "University of Edinburgh, Edinburgh, Scotland",
-    55.9441283,
-    -3.1884063612377216
-  ]
-  [
     "Universit\u00e9 Catholique de Louvain, Louvain-la-Neuve, Belgium",
-    50.669323,
-	4.616068
+    50.6682538,
+    4.620109526811595
   ],
   [
     "Technical University of Munich, Munich, Germany",
-    48.149653,
-	11.567560
+    48.149554550000005,
+    11.567753141783761
   ],
   [
-    "University of London, London, United Kingdom",
-    51.520832,
-	-0.129239
+    "Universit\u00e9 de Montr\u00e9al, Montreal",
+    45.504956750000005,
+    -73.61453887024297
   ],
   [
-    "Washington, United States of America",
-    38.904206,
-	-77.023344
+    "Fields Institute, Toronto, Canada",
+    43.65879595,
+    -79.39755040601011
+  ],
+  [
+    "Washington, US",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "University of London, London, UK",
+    51.5276441,
+    -0.10359892518182634
+  ],
+  [
+    "University of Edinburgh, Edinburgh, Scotland",
+    55.94407645,
+    -3.1883735563964555
+  ],
+  [
+    "University of Zagreb, Zagreb, Croatia",
+    45.81689475,
+    15.897824045977195
   ]
 ];
