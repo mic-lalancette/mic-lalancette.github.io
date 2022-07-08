b8c81d3a9bd2d0a93224ca983ab8211076ca8ebb
MicLalancette <61370608+mic-lalancette@users.noreply.github.com>
2022-07-08 00:11:05 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,52 @@
 var addressPoints = [
   [
-    "University of Zagreb, Zagreb, Croatia",
-    45.8644842,
-    15.8005897
-  ],
-  [
-    "Fields Institute, Toronto, Canada",
-    43.65879595,
-    -79.39755040601011
-  ],
-  [
-    "Fields Institute, Toronto, Canada",
-    43.65879595,
-    -79.39755040601011
-  ],
-  [
     "McGill University, Montr\u00e9al, Canada",
     45.50691775,
     -73.5791162596496
   ],
   [
+    "Fields Institute, Toronto, Canada",
+    43.65879595,
+    -79.39755040601011
+  ],
+  [
+    "University of Zagreb, Zagreb, Croatia",
+    45.8644842,
+    15.8005897
+  ],
+  [
+    "Universit\u00e9 de Montr\u00e9al, Montr\u00e9al, Canada",
+    45.505536,
+	-73.613864
+  ],
+  [
+    "Fields Institute, Toronto, Canada",
+    43.65879595,
+    -79.39755040601011
+  ],
+  [
     "University of Edinburgh, Edinburgh, Scotland",
     55.9441283,
     -3.1884063612377216
+  ]
+  [
+    "Universit\u00e9 Catholique de Louvain, Louvain-la-Neuve, Belgium",
+    50.669323,
+	4.616068
+  ],
+  [
+    "Technical University of Munich, Munich, Germany",
+    48.149653,
+	11.567560
+  ],
+  [
+    "University of London, London, United Kingdom",
+    51.520832,
+	-0.129239
+  ],
+  [
+    "Washington, United States of America",
+    38.904206,
+	-77.023344
   ]
 ];
