f01bda82a44e39c08340cc36dd352ba3b7da7d17
MicLalancette <61370608+mic-lalancette@users.noreply.github.com>
2023-03-11 11:33:21 +0100

    add recent talks on the map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,28 +1,33 @@
 var addressPoints = [
   [
-    "McGill University, Montr\u00e9al, Canada",
-    45.50691775,
-    -73.5791162596496
+   "Bocconi University, Milan, Italy"
+   45.449010,
+   9.191079
   ],
   [
-    "Universit\u00e9 Catholique de Louvain, Louvain-la-Neuve, Belgium",
-    50.6682538,
-    4.620109526811595
+	"University of Duisburg-Essen, Essen, Germany",
+	51.463438,
+	7.005634
+  ],
+  [
+    "King's College London, London, UK",
+    51.512030,
+	-0.116388
+  ],
+  [
+	"Universit\u00e9 de Gen\u00e8ve, Gen\u00e8ve, Switzerland",
+	46.195518,
+	6.140247
+  ],
+  [
+    "Raitenhaslach, Burghausen, Germany",
+    48.128620,
+	12.787079
   ],
   [
     "Technical University of Munich, Munich, Germany",
     48.149554550000005,
     11.567753141783761
-  ],
-  [
-    "Universit\u00e9 de Montr\u00e9al, Montreal",
-    45.504956750000005,
-    -73.61453887024297
-  ],
-  [
-    "Fields Institute, Toronto, Canada",
-    43.65879595,
-    -79.39755040601011
   ],
   [
     "Washington, US",
@@ -35,13 +40,43 @@ var addressPoints = [
     -0.10359892518182634
   ],
   [
+    "Technical University of Munich, Munich, Germany",
+    48.149554550000005,
+    11.567753141783761
+  ],
+  [
+    "Universit\u00e9 Catholique de Louvain, Louvain-la-Neuve, Belgium",
+    50.6682538,
+    4.620109526811595
+  ],
+  [
     "University of Edinburgh, Edinburgh, Scotland",
     55.94407645,
     -3.1883735563964555
   ],
   [
+    "Fields Institute, Toronto, Canada",
+    43.65879595,
+    -79.39755040601011
+  ],
+  [
+    "Universit\u00e9 de Montr\u00e9al, Montr\u00e9al",
+    45.504956750000005,
+    -73.61453887024297
+  ],
+  [
     "University of Zagreb, Zagreb, Croatia",
     45.81689475,
     15.897824045977195
+  ],
+  [
+    "Fields Institute, Toronto, Canada",
+    43.65879595,
+    -79.39755040601011
+  ],
+  [
+    "McGill University, Montr\u00e9al, Canada",
+    45.50691775,
+    -73.5791162596496
   ]
 ];
